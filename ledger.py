662b4b69aef5e57593c7a4375b3e4debf01e74b7
Craig Hammond <80643440+CompleteSuspect@users.noreply.github.com>
2025-09-05 20:27:09 +0100

    first commit

---
+++ ledger.py
@@ -0,0 +1,30 @@
+balances = {"Alice": 500,
+            "Bob": 1000,
+            "Craig": 750
+            }
+
+with open("transactions.txt", "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    if not line or not line.startswith('#'):
+        new_line = line.split('#', 1)[0].strip()
+        try:
+            name, amount_str = new_line.split(',', 1)
+            name = name.title().strip()
+            amount_int = int(amount_str.strip())
+            print(f'Fetching from file: {name}, {amount_int}')
+
+            if balances.get(name, 0) + amount_int > 0:
+                balances[name] = balances.get(name, 0) + amount_int
+                print(f'Calculating new balance for {name}: £{balances[name]}\n')
+            else:
+                print(f"{name}'s balance is too low(£{balances[name]}), transaction aborted!\n")
+
+        except ValueError as e:
+            print(f'Skipping line, {e}')
+
+with open('updated_balances.txt', 'w') as f:
+    for name, balance in balances.items():
+        f.write(f'{name}, {balance}\n')
+        print(f'{name}, has £{balance}.')
